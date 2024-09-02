a366d71671221b56ef35d02bfec134390fea206b
emadjshah <emadshah14@gmail.com>
2024-09-03 00:40:31 +0530

    More Files

---
+++ 24 Javascript/01 Starting-Javascript/02 Variables/script.js
@@ -0,0 +1,23 @@
+//!----- Using `var`----
+
+//Task: Storing a person's first name
+
+// var firstName = "Emad";
+// console.log(firstName);
+
+//!-----Using `let`----
+
+//Task: Storing a person's first name
+
+  let firstName = "Emad";
+console.log(firstName);
+
+//!-----Using `const`----
+
+//Task: Storing a person's first name
+
+// const firstName = "Emad";
+// console.log(firstName);
+
+
+// Remove comment for the one you want to use.
